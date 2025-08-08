6ddb58e7348f415e0d37dfcfeef93818021f47ab
Kartik Goyal <kartikcodes137@gmail.com>
2025-08-08 14:52:20 +0530

    add chef auth and admin auth middlewar

--- MVC_backend/pkg/controllers/cart.go
+++ MVC_backend/pkg/controllers/cart.go
@@ -20,6 +20,7 @@ func NewCartHandler(store types.CartStore, userStore types.UserStore) *CartHandl
 
 func (h *CartHandler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/addtocart", auth.JWTauth(h.AddToCartHandler, h.userStore)).Methods("POST")
+	router.HandleFunc("/editcart", auth.JWTauth(h.UpdateCartHandler, h.userStore)).Methods("POST")
 	router.HandleFunc("/getcart/{orderid}", auth.JWTauth(h.GetCartItemsHandler, h.userStore)).Methods("GET")
 }
 
@@ -38,6 +39,23 @@ func (h *CartHandler) AddToCartHandler(w http.ResponseWriter, r *http.Request) {
 			utils.WriteError(w, http.StatusBadRequest, err2)
 			return
 		}
+    }
+
+    utils.WriteJSON(w, http.StatusCreated, nil)
+}
+
+func (h *CartHandler) UpdateCartHandler(w http.ResponseWriter, r *http.Request) {
+
+    var item types.CartItem
+    if err := utils.ParseJSON(r, &item); err != nil {
+        utils.WriteError(w, http.StatusBadRequest, err)
+        return
+    }
+
+    err := h.store.UpdateCartItemQuantity(item)
+    if err != nil { 
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
     }
 
     utils.WriteJSON(w, http.StatusCreated, nil)
---
+++ MVC_backend/pkg/middleware/chef.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/kartikgoyal137/MVC/pkg/types"
+	"github.com/kartikgoyal137/MVC/pkg/utils"
+)
+
+func ChefAuth(handlerFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
+   return func(w http.ResponseWriter, r *http.Request) {
+
+	userID, ok := r.Context().Value(UserKey).(int)
+	if !ok {
+			utils.WriteError(w, http.StatusNotFound, fmt.Errorf("user not found"))
+			return
+	}
+
+	user, err := store.GetUserById(userID)
+	if err != nil {
+			utils.WriteError(w, http.StatusNotFound, fmt.Errorf("user not found"))
+			return
+	}
+
+	if user.Role != "chef" {
+			utils.WriteError(w, http.StatusNotFound, fmt.Errorf("user not authorized"))
+			return
+		}
+
+    handlerFunc(w, r)
+   }
+}
